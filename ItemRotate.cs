479a2eb68ae057ff06a3162d075a74deadac5fab
C093069 김은지 <86828290+OceanFlavorStarCandy@users.noreply.github.com>
2022-05-27 15:08:49 +0900

    아이템 빙글빙글
    아이템 습득은 그냥 넣어둔거에요 필요없으면 지우시면 될 것 같아여

---
+++ ItemRotate.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemRotate : MonoBehaviour
+{
+    float rotSpeed = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 아이템 빙글빙글
+        transform.Rotate(new Vector3(0, rotSpeed * Time.deltaTime, 0));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player") // 플레이어가 아이템에 닿으면 아이템 획득(나중에 상호작용 키 입력으로 수정 필요)
+        {
+            Debug.Log("Item Obtained");
+            Destroy(gameObject);
+        }
+    }
+}
