f7ba4854ea52fb1106f438cc71c6bff3dacc25cb
kimhyenjung <48229667+kimhyenjung@users.noreply.github.com>
2022-05-28 11:01:47 +0900

    Player 달리기 걷기
    플레이어 달리기 걷기 , 달리기 시 HP 감소, 걸을 시 HP 증가
    
    조금 부자연스러움

---
+++ HP.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HP : MonoBehaviour
+{
+    public Slider hpbar;
+    public float maxHp;
+    public float currenthp;
+    bool runing;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hpbar.value = currenthp / maxHp;
+
+        if (GameObject.Find("Player").GetComponent<Player>().IsRuning == true)
+        {
+            Debug.Log("IsRuning");
+            currenthp -= 1;
+        }
+
+        if (GameObject.Find("Player").GetComponent<Player>().IsRuning == false)
+        {
+            if (currenthp <= maxHp)
+            {
+                Debug.Log("Is'nt runing");
+                currenthp += 0.1f;
+            }
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    float speed, MoveX, MoveY;
+    [SerializeField] float normalSpeed, runSpeed;
+    float hp;
+
+    public bool IsRuning;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsRuning = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if(Input.GetKey(KeyCode.LeftShift))
+        { 
+
+            if (GameObject.Find("HP_Bar").GetComponent<HP>().currenthp <= 5)
+            {
+                speed = normalSpeed;
+                IsRuning = false;
+            }
+
+            else if(GameObject.Find("HP_Bar").GetComponent<HP>().currenthp >= 15)
+            {
+
+                speed = runSpeed;
+                IsRuning = true;
+            }
+
+        }    
+        else
+        {
+            speed = normalSpeed;
+            IsRuning = false;
+        }
+
+        MoveX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        MoveY = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Translate(MoveX, 0, MoveY);
+    }
+}
