321e9aebe4488ef258f7b6e3da20d7bb20aae5f4
H0NEYC0RN <87184810+H0NEYC0RN@users.noreply.github.com>
2022-05-30 11:46:27 +0900

    [0530] Player Fix
    [수정사항]
    - Player가 Hide상태일때, 자유롭게 이동 가능한 문제 발생
    → Player가 Hide상태가 아닐때에만 이동 관련 함수 작동하게 변경

--- Player.cs
+++ Player.cs
@@ -7,27 +7,23 @@ public class Player : MonoBehaviour
 {
     float speed, MoveX, MoveY;
     [SerializeField] float normalSpeed, runSpeed;
+    
     float hp;
+    public bool IsRuning = false;
+    
+    [SerializeField] GameObject State;
 
-    public bool IsRuning;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        IsRuning = false;
-    }
-
-    // Update is called once per frame
     void Update()
     {
         Movement();
     }
 
+
     void Movement()
     {
         if(Input.GetKey(KeyCode.LeftShift))
         { 
-
             if (GameObject.Find("HP_Bar").GetComponent<HP>().currenthp <= 5)
             {
                 speed = normalSpeed;
@@ -36,30 +32,31 @@ public class Player : MonoBehaviour
 
             else if(GameObject.Find("HP_Bar").GetComponent<HP>().currenthp >= 15)
             {
-
                 speed = runSpeed;
                 IsRuning = true;
             }
-
         }    
         else
         {
             speed = normalSpeed;
             IsRuning = false;
         }
+        
+        if (State.GetComponent<Player_state>().isHided == false)
+        {
+            MoveX = Input.GetAxis("Horizontal");
+            MoveY = Input.GetAxis("Vertical");
 
-        MoveX = Input.GetAxis("Horizontal");
-        MoveY = Input.GetAxis("Vertical");
+            // 캐릭터가 Z축(인스펙터상 Y축)으로 움직이지 않도록 고정
+            Vector3 dir = new Vector3(MoveX, 0, MoveY);
+            dir.Normalize(); // 정규화
 
-        // 캐릭터가 Z축(인스펙터상 Y축)으로 움직이지 않도록 고정
-        Vector3 dir = new Vector3(MoveX, 0, MoveY); 
-        dir.Normalize(); // 정규화
+            // 현재 (메인)카메라가 바라보는 방향으로 이동
+            dir = Camera.main.transform.TransformDirection(dir);
+            // Player 가 공중, 바닥으로 이동하지 않도록 y(z)값을 고정
+            dir.y = 0;
 
-        // 현재 (메인)카메라가 바라보는 방향으로 이동
-        dir = Camera.main.transform.TransformDirection(dir);
-        // Player 가 공중, 바닥으로 이동하지 않도록 y(z)값을 고정
-        dir.y = 0;
-
-        transform.position += dir * speed * Time.deltaTime;
+            transform.position += dir * speed * Time.deltaTime;
+        }
     }
 }
