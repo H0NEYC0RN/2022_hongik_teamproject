969c69e3f2fea1d771a3b9913dd2097c68f658e0
C093069 김은지 <86828290+OceanFlavorStarCandy@users.noreply.github.com>
2022-06-18 21:31:16 +0900

    0618 Opening Scene
    나중에 오프닝 언제 나오게할지 조건만 추가해주시면 될 것 같아요
    오프닝 컷씬이 동작하게만 만들어뒀습니다

---
+++ OpeningScene.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpeningScene : MonoBehaviour
+{
+    // OpenScene 캔버스와 그 자식 UI들에 전부 태그 각각 달아줄 것(이름과 같은 태그)
+    public GameObject Opening; // OpeningScene Canvas
+    public GameObject OP1; // Opening01
+    public GameObject OP2; // Opening02
+    public GameObject OP3; // Opening03
+    int enterCount = 0; // 숫자 카운트로 순서를 정해줄 것
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Opening = GameObject.FindWithTag("OpenScene");
+        OP1 = GameObject.FindWithTag("Opening01");
+        OP2 = GameObject.FindWithTag("Opening02");
+        OP3 = GameObject.FindWithTag("Opening03");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (enterCount == 0)
+            {
+                Debug.Log("SpaceBar Pressed");
+                OP1.SetActive(false);
+                enterCount++;
+            }
+
+            else if (enterCount == 1)
+            {
+                Debug.Log("SpaceBar Pressed");
+                OP2.SetActive(false);
+                enterCount++;
+            }
+
+            else if (enterCount == 2)
+            {
+                Debug.Log("SpaceBar Pressed");
+                OP3.SetActive(false);
+                enterCount++;
+            }
+
+            else if (enterCount == 3)
+            {
+                Debug.Log("SpaceBar Pressed");
+                Opening.SetActive(false);
+            }
+        }
+        
+    }
+}
