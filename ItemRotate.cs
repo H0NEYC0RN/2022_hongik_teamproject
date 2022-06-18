259458ad0469bc37fde1dbe3eb9a45c2d3284485
H0NEYC0RN <87184810+H0NEYC0RN@users.noreply.github.com>
2022-06-19 02:31:47 +0900

    [0619] ItemRotate Fix
    아이템 줍기 및 오브젝트 파괴 관련 기능이 타 스크립트에 구현되어 있어
    충돌하는 현상 방지 위해 rotate 기능 외 삭제

--- ItemRotate.cs
+++ ItemRotate.cs
@@ -6,37 +6,8 @@ public class ItemRotate : MonoBehaviour
 {
     float rotSpeed = 50f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        // 아이템 빙글빙글
         transform.Rotate(new Vector3(0, rotSpeed * Time.deltaTime, 0));
-        
-        // 스페이스바로 물건 줍기
-        if (isPickup && Input.GetKeyDown(KeyCode.Space))
-        {
-            PickUp();
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Player") // 플레이어가 아이템에 닿으면 아이템 획득(나중에 상호작용 키 입력으로 수정 필요)
-        {
-            Debug.Log("Item Obtained");
-            isPickup = true;
-        }
-    }
-    
-    public void PickUp()
-    {
-        pickUpText.gameObject.SetActive(true);
-        Destroy(gameObject);
     }
 }
